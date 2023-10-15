235f4071ae5f52d3e1e977995608a99a275f4028
Otavio Augusto Mendonça <87283267+LegendaryEagle00@users.noreply.github.com>
2023-10-15 06:21:36 -0300

    inicio

---
+++ src/main/java/com/github/leagle00/logica/EstruturaDeDecisao.java
@@ -0,0 +1,16 @@
+package com.github.leagle00.logica;
+
+public class EstruturaDeDecisao {
+    public static void main(String[] args) {
+        if (condiçaoBooleana){
+            // executa codigo quando condiçao for verdadeira
+            codigo;
+        } else if (condicaoBooleanaN) {
+            // executa codigo quando condiçao 'n' for verdadeira
+            codigo;
+        }else{
+            //executa codigo quando falso
+            codigo;
+        }
+    }
+}
